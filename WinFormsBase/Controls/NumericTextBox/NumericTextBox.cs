f01c633fff7292b2757dd6b71ff4cbb7c54ec4e0
dcm4n <docmanhatan10@gmail.com>
2025-09-14 21:57:06 +0200

    Added numeric textbox

---
+++ WinFormsBase/Controls/NumericTextBox/NumericTextBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace WinFormsBase.Controls.NumericTextBox
+{
+    public class NumericTextBox : NumericUpDown
+    {
+        public NumericTextBox()
+        {
+            Controls[0].Visible = false;
+            Maximum = Decimal.MaxValue;
+            DecimalPlaces = 2;
+        }
+
+        protected override void OnTextBoxResize(object? source, EventArgs e)
+        {
+            Controls[1].Width = Width - 4;
+        }
+    }
+}
