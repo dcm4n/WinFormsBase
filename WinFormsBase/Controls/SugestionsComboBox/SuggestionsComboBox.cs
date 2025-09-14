f2c2a110a0a7a19643342cbe3732aff03753e6a1
dcm4n <docmanhatan10@gmail.com>
2025-09-14 21:57:48 +0200

    Added suggestionsComboBox

---
+++ WinFormsBase/Controls/SugestionsComboBox/SuggestionsComboBox.cs
@@ -0,0 +1,28 @@
+﻿
+namespace WinFormsBase.Controls.SugestionsComboBox
+{
+    public class SuggestionsComboBox : ComboBox
+    {
+        public SuggestionsComboBox()
+        {
+            AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            AutoCompleteSource = AutoCompleteSource.ListItems;
+            FormattingEnabled = true;
+            //SelectedIndex = -1;
+        }
+
+        // Prevent preselection of first item
+        protected override void OnBindingContextChanged(EventArgs e)
+        {
+            SelectedIndex = -1;
+            base.OnBindingContextChanged(e);
+        }
+
+        // Hide DropDown list. Show only suggestions
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            DroppedDown = false;
+            base.OnKeyDown(e);        
+        }
+    }
+}
