117c61bf8c3b604b154b61fa787b254046f11a72
dcm4n <docmanhatan10@gmail.com>
2025-09-14 21:55:45 +0200

    added SuperGrid UserControl

---
+++ WinFormsBase/Controls/SuperGrid/SuperGrid.Designer.cs
@@ -0,0 +1,66 @@
+﻿using DevComponents.DotNetBar.SuperGrid;
+using DevComponents.DotNetBar;
+using DevComponents.DotNetBar.SuperGrid.Style;
+
+namespace WinFormsBase.Controls
+{
+    partial class SuperGrid
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuperGridView = new SuperGridControl();
+            SuspendLayout();
+            // 
+            // SuperGridView
+            // 
+            SuperGridView.BackColor = Color.White;
+            SuperGridView.Dock = DockStyle.Fill;
+            SuperGridView.ForeColor = Color.Black;
+            SuperGridView.Location = new Point(0, 0);
+            SuperGridView.Name = "SuperDataGridView";
+            SuperGridView.PrimaryGrid.EnableColumnFiltering = true;
+            SuperGridView.PrimaryGrid.EnableFiltering = true;
+            SuperGridView.PrimaryGrid.Filter.Visible = true;
+            SuperGridView.PrimaryGrid.FilterMatchType = FilterMatchType.RegularExpressions;
+            SuperGridView.Size = new Size(150, 150);
+            SuperGridView.TabIndex = 0;
+            SuperGridView.GetCellStyle += SuperGridView_GetCellStyle;
+            // 
+            // SuperGrid
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(SuperGridView);
+            Name = "SuperGrid";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        public SuperGridControl SuperGridView;
+    }
+}
---
+++ WinFormsBase/Controls/SuperGrid/SuperGrid.cs
@@ -0,0 +1,36 @@
+﻿using DevComponents.DotNetBar;
+using DevComponents.DotNetBar.SuperGrid;
+using System.Data;
+
+namespace WinFormsBase.Controls
+{
+    public partial class SuperGrid : UserControl
+    {
+        public SuperGrid()
+        {
+            InitializeComponent();
+
+            // Theme
+            StyleManager styleManager = new StyleManager();
+            styleManager.ManagerStyle = eStyle.Metro;
+
+            // Initial style
+            Dock = DockStyle.Fill;
+            SuperGridView.PrimaryGrid.GridPanel.ColumnHeader.FilterImageVisibility = ImageVisibility.Never;
+
+            // Preload empty datatable
+            DataTable dummyDataTable = new DataTable();
+            dummyDataTable.Rows.Add();
+            SuperGridView.PrimaryGrid.DataSource = dummyDataTable;
+        }
+
+        private void SuperGridView_GetCellStyle(object sender, GridGetCellStyleEventArgs e)
+        {
+            // Center checkbox in CheckBox columns
+            if (e.GridCell?.GridColumn.EditorType == typeof(GridCheckBoxXEditControl))
+            {
+                e.Style.Alignment = DevComponents.DotNetBar.SuperGrid.Style.Alignment.MiddleCenter;
+            }
+        }
+    }
+}
